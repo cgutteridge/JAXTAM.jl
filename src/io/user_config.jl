01fc1daa80f43b6ed5d749cb343f06f923b5285e
Robert Rosca <robertrosca@users.noreply.github.com>
2018-05-11 14:59:54 +0100

    Make config values more general, add support for default mission for master loading

--- src/io/user_config.jl
+++ src/io/user_config.jl
@@ -13,7 +13,7 @@ function _config_load(config_path=string(pwd(), "/user_configs.jld2"))
     return load(config_path, "config_data")
 end
 
-function _config_edit(mission_name::String, mission_path::String;
+function _config_edit(key_name::String, key_value::String;
         config_path=string(pwd(), "/user_configs.jld2"))
 
     if !isfile(config_path)
@@ -23,12 +23,12 @@ function _config_edit(mission_name::String, mission_path::String;
     config_data = _config_load(config_path)
 
     config_data["_config_edit_date"] = string(Dates.DateTime(now()))
-    config_data[mission_name] = mission_path
+    config_data[key_name] = key_value
 
     save(config_path, Dict("config_data" => config_data))
 end
 
-function _config_rm(mission_name::String;
+function _config_rm(key_name::String;
         config_path=string(pwd(), "/user_configs.jld2"))
 
     if !isfile(config_path)
@@ -37,33 +37,33 @@ function _config_rm(mission_name::String;
 
     config_data = _config_load(config_path)
 
-    delete!(config_data, mission_name)
+    delete!(config_data, key_name)
     config_data["_config_edit_date"] = string(Dates.DateTime(now()))
 
     save(config_path, Dict("config_data" => config_data))
 end
 
-function _config_mission_path(mission_name::Union{String,Symbol}, config_path=string(pwd(), "/user_configs.jld2"))
+function _config_key_value(key_name::Union{String,Symbol}, config_path=string(pwd(), "/user_configs.jld2"))
     config_data = _config_load(config_path)
 
-    return config_data[string(mission_name)]
+    return config_data[string(key_name)]
 end
 
 function config()
     return _config_load()
 end
 
-function config(mission_name::Union{String,Symbol}, mission_path::String)
-    _config_edit(String(mission_name), mission_path)
+function config(key_name::Union{String,Symbol}, key_value::Union{String,Symbol})
+    _config_edit(String(key_name), String(key_value))
     return _config_load()
 end
 
-function config(mission_name::Union{String,Symbol})
-    return _config_load()[String(mission_name)]
+function config(key_name::Union{String,Symbol})
+    return _config_load()[String(key_name)]
 end
 
-function config_rm(mission_name::Union{String,Symbol})
-    info("Removing \"$mission_name => $(_config_load()[String(mission_name)])\" from config file")
-    _config_rm(String(mission_name))
+function config_rm(key_name::Union{String,Symbol})
+    info("Removing \"$key_name => $(_config_load()[String(key_name)])\" from config file")
+    _config_rm(String(key_name))
     return _config_load()
 end
