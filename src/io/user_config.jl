b7d6149c6396ebd3455191d3632abcd24a8e697a
RobertRosca <RobertRosca@users.noreply.github.com>
2018-06-05 19:53:30 +0100

    Documentation for user_config.jl

--- src/io/user_config.jl
+++ src/io/user_config.jl
@@ -17,10 +17,22 @@ function _config_gen(config_path::String=string(pwd(), "/user_configs.jld2"))
     save(config_path, Dict("config_data" => config_data))
 end
 
+"""
+    _config_load(config_path=string(pwd(), "/user_configs.jld2"))
+
+Loads data from the configuration file.
+"""
 function _config_load(config_path=string(pwd(), "/user_configs.jld2"))
     return load(config_path, "config_data")
 end
 
+"""
+    _config_edit(key_name::String, key_value::String;
+            config_path=string(pwd(), "/user_configs.jld2"))
+
+Edit configuration file, automatically changes the `_config_edit_date`
+value.
+"""
 function _config_edit(key_name::String, key_value::String;
         config_path=string(pwd(), "/user_configs.jld2"))
 
@@ -36,6 +48,14 @@ function _config_edit(key_name::String, key_value::String;
     save(config_path, Dict("config_data" => config_data))
 end
 
+
+"""
+    _config_rm(key_name::String;
+            config_path=string(pwd(), "/user_configs.jld2"))
+
+Removes a the key `key_name` from the configuration file,
+then saves the changes.
+"""
 function _config_rm(key_name::String;
         config_path=string(pwd(), "/user_configs.jld2"))
 
@@ -51,25 +71,53 @@ function _config_rm(key_name::String;
     save(config_path, Dict("config_data" => config_data))
 end
 
+"""
+    _config_key_value(key_name::Union{String,Symbol}, config_path=string(pwd(), "/user_configs.jld2"))
+        config_data = _config_load(config_path)
+
+Loads and returns the value for `key_name`
+"""
 function _config_key_value(key_name::Union{String,Symbol}, config_path=string(pwd(), "/user_configs.jld2"))
     config_data = _config_load(config_path)
 
     return config_data[string(key_name)]
 end
 
+"""
+    config()
+
+Returns the full configuration file data
+"""
 function config()
     return _config_load()
 end
 
+"""
+    config(key_name::Union{String,Symbol}, key_value::Union{String,Symbol})
+        _config_edit(String(key_name), String(key_value))
+
+Adds `key_name` with `key_value` to the configuration file and saves
+"""
 function config(key_name::Union{String,Symbol}, key_value::Union{String,Symbol})
     _config_edit(String(key_name), String(key_value))
     return _config_load()
 end
 
+
+"""
+    config(key_name::Union{String,Symbol})
+
+Returns the value of `key_name`
+"""
 function config(key_name::Union{String,Symbol})
     return _config_load()[String(key_name)]
 end
 
+"""
+    config_rm(key_name::Union{String,Symbol})
+
+Removes `key_name` from the configuration file and saves changes.
+"""
 function config_rm(key_name::Union{String,Symbol})
     info("Removing \"$key_name => $(_config_load()[String(key_name)])\" from config file")
     _config_rm(String(key_name))
