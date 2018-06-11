35c6eef35cee827e6a259c9e7c3b64534815da73
RobertRosca <RobertRosca@users.noreply.github.com>
2018-06-11 17:58:02 +0100

    Changing config functionality

--- src/JAXTAM.jl
+++ src/JAXTAM.jl
@@ -9,11 +9,13 @@ using JLD
 using Query
 using FTPClient
 using LightXML
+using Compat
 
 include("missions/mission_control.jl")
 include("missions/default_missions.jl")
 include("io/user_config.jl")
 include("io/master_tables.jl")
+include("io/master_append.jl")
 include("io/misc.jl")
 include("io/data_download.jl")
 
