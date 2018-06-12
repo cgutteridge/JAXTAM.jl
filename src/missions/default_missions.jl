463b386ae43a8dd549a245aac9741b5959e8228f
Robert Rosca <robertrosca@users.noreply.github.com>
2018-06-12 03:49:30 +0100

    Improve config, store expression not function

--- src/missions/default_missions.jl
+++ src/missions/default_missions.jl
@@ -4,18 +4,22 @@
 Function returning dictionary of some pre-set HEASARC missions,
 using the `MissionDefinition` type. Name and heasarc url pre-set,
 mission path is left as blank string
+
+NB: JLD cannot save functions as of 0.5.0+, instead an expression
+is saved, which then calls a function in the JAXTAM package. Thus send
+a PR if you want another dir function to be added to JAXTAM
 """
 function _get_default_missions()
     mission_nicer = MissionDefinition("nicer",
         "https://heasarc.gsfc.nasa.gov/FTP/heasarc/dbase/tdat_files/heasarc_nicermastr.tdat.gz",
         "mission_path",
-        _nicer_observation_dir
+        :(JAXTAM._nicer_observation_dir)
     )
 
     mission_nustar = MissionDefinition("nustar",
         "https://heasarc.gsfc.nasa.gov/FTP/heasarc/dbase/tdat_files/heasarc_numaster.tdat.gz",
         "mission_path",
-        _nustar_observation_dir
+        :(JAXTAM._nustar_observation_dir)
     )
 
     return Dict(:nicer => mission_nicer, :nustar => mission_nustar)
@@ -28,7 +32,8 @@ function _nicer_observation_dir(obsid::String, mjd_day::String)
     date_year = date_time[1:4]
     date_month = date_time[6:7]
 
-    folder_path = string("/.nicer_archive/.nicer_$(date_year)$(date_month)a/obs/$(date_year)_$(date_month)/$obsid")
+    folder_path = string("/.nicer_archive/.nicer_$(date_year)$(date_month)a/",
+                            "obs/$(date_year)_$(date_month)/$obsid")
 
     return folder_path
 end
