e8e67d1e1666983e954cb1e07890e994e4b6a243
Robert Rosca <robertrosca@users.noreply.github.com>
2018-06-12 01:20:43 +0100

    Symbolify config functions

--- src/io/user_config.jl
+++ src/io/user_config.jl
@@ -42,7 +42,7 @@ end
 Edit configuration file, automatically changes the `_config_edit_date`
 value.
 """
-function _config_edit(key_name::String, key_value;
+function _config_edit(key_name::Symbol, key_value;
         config_path=string(Pkg.dir(), "/JAXTAM/user_configs.jld"))
 
     if !isfile(config_path)
@@ -51,7 +51,7 @@ function _config_edit(key_name::String, key_value;
 
     config_data = _config_load(config_path)
 
-    config_data["_config_edit_date"] = Dates.DateTime(now())
+    config_data[:_config_edit_date] = Dates.DateTime(now())
     config_data[key_name] = key_value
 
     save(config_path, Dict("config_data" => config_data))
@@ -65,7 +65,7 @@ end
 Removes a the key `key_name` from the configuration file,
 then saves the changes.
 """
-function _config_rm(key_name::String;
+function _config_rm(key_name::Symbol;
         config_path=string(Pkg.dir(), "/JAXTAM/user_configs.jld"))
 
     if !isfile(config_path)
@@ -86,10 +86,10 @@ end
 
 Loads and returns the value for `key_name`
 """
-function _config_key_value(key_name::Union{String,Symbol}, config_path=string(Pkg.dir(), "/JAXTAM/user_configs.jld"))
+function _config_key_value(key_name::Symbol, config_path=string(Pkg.dir(), "/JAXTAM/user_configs.jld"))
     config_data = _config_load(config_path)
 
-    return config_data[string(key_name)]
+    return config_data[key_name]
 end
 
 """
@@ -107,22 +107,22 @@ end
 
 Adds `key_name` with `key_value` to the configuration file and saves
 """
-function config(key_name::Union{String,Symbol}, key_value::Union{String,Symbol,MissionDefinition})
+function config(key_name::Symbol, key_value::Union{String,Symbol,MissionDefinition})
     if typeof(key_value) == MissionDefinition
-        _config_edit(String(key_name), key_value)
+        _config_edit(key_name, key_value)
     else
         defaults = _get_default_missions()
 
-        if string(key_name) in keys(defaults)
+        if key_name in keys(defaults)
             info("$key_name found in defaults\nUsing $key_value as path")
-            mission = defaults[string(key_name)]
+            mission = defaults[key_name]
             mission.path = key_value
-
-            _config_edit(String(key_name), mission)
+            
+            _config_edit(key_name, mission)
         else
             info("$key_name not found in defaults, treated as a string")
             info("If $key_name is a mission, use config(key_name, MissionDefinition) to fully set up the mission variables")
-            _config_edit(string(key_name), String(key_value))
+            _config_edit(key_name, key_value)
         end
     end
 
@@ -135,11 +135,11 @@ end
 
 Returns the value of `key_name`
 """
-function config(key_name::Union{String,Symbol})
+function config(key_name::Symbol)
     if key_name == "default" || key_name == :default
-        return _config_load()[string( _config_load()["default"])]
+        return _config_load()[_config_load()[:default]]
     else
-        return _config_load()[String(key_name)]
+        return _config_load()[key_name]
     end
 end
 
@@ -148,8 +148,8 @@ end
 
 Removes `key_name` from the configuration file and saves changes.
 """
-function config_rm(key_name::Union{String,Symbol})
-    info("Removing \"$key_name => $(_config_load()[String(key_name)])\" from config file")
-    _config_rm(String(key_name))
+function config_rm(key_name::Symbol)
+    info("Removing \"$key_name => $(_config_load()[key_name])\" from config file")
+    _config_rm(key_name)
     return _config_load()
 end
