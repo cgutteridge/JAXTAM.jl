1249e09be86d0a07d18a8d41e2b8336494b2af22
Robert Rosca <robertrosca@users.noreply.github.com>
2018-06-18 19:04:00 +0100

    Add user read_cl functions

--- src/io/read_events.jl
+++ src/io/read_events.jl
@@ -1,3 +1,9 @@
+struct InstrumentData
+    instrument::Symbol
+    events::DataFrame
+    gti::DataFrame
+end
+
 function _read_fits_hdu(fits_file, hdu_id)
     fits_cols_events = FITSIO.colnames(fits_file[hdu_id])
 
@@ -19,8 +25,10 @@ function _read_fits_hdu(fits_file, hdu_id)
 end
 
 function _read_fits_event(fits_path)
-    info("Loading $fits_path")
+    print("\n"); info("Loading $fits_path")
     fits_file   = FITS(fits_path)
+
+    instrument_name = read_header(fits_file[1])["INSTRUME"]
 
     fits_events_df = _read_fits_hdu(fits_file, 2)
 
@@ -28,7 +36,7 @@ function _read_fits_event(fits_path)
 
     close(fits_file)
 
-    return fits_events_df, fits_gti_df
+    return InstrumentData(instrument_name, fits_events_df, fits_gti_df)
 end
 
 function _save_fits_feather(feather_dir, instrument_name, fits_events_df, fits_gti_df)
@@ -36,8 +44,79 @@ function _save_fits_feather(feather_dir, instrument_name, fits_events_df, fits_g
     Feather.write(joinpath(feather_dir, "$instrument_name\_gtis.feather"), fits_gti_df)
 end
 
-function _process_fits_event(fits_path, output_dir, instrument name)
-    events, gti = _read_fits_event(fits_path)
+function read_cl_fits(mission_name, obs_row, obsid)
+    file_path = [i for i in obs_row[:event_cl][1]] # Convert tuple to array
+    
+    files = []
+    
+    for file in file_path
+        if isfile(file)
+            append!(files, [files])
+            info("Found: $file")
+        elseif isfile(string(file, ".gz")) # Check for files ending in .evt.gz too
+            info("Found: $(string(file, ".gz"))")
+            append!(files, [string(file, ".gz")])
+        else
+            warn("NOT found: $file")
+        end
+    end
 
-    _save_fits_feather(output_dir, instrument_name, events, gti)
+    file_no = length(files)
+
+    print("\n"); info("Found $file_no file(s) for $obsid")
+
+    if file_no == 1
+        instrument_data = _read_fits_event(files[1])
+        return Dict(instrument_data.instrument => instrument_data)
+    elseif file_no > 1
+        per_instrument = Dict{Symbol,InstrumentData}()
+        for file in files
+            instrument_data = _read_fits_event(file)
+            per_instrument[instrument_data.instrument] = instrument_data
+        end
+
+        return per_instrument
+    end
+end
+
+function read_cl(mission_name, append_df, obsid)
+    obs_row     = master_query(append_df, :obsid, obsid)
+    JAXTAM_path = abspath(string(obs_row[:obs_path][1], "/JAXTAM/"))
+
+    if !isdir(obs_row[:obs_path][1])
+        error("$(obs_row[:obs_path][1]) not found")
+    end
+
+    if !isdir(JAXTAM_path)
+        mkdir(JAXTAM_path)
+    end
+
+    JAXTAM_content = readdir(JAXTAM_path)
+    JAXTAM_e_files = count(contains.(JAXTAM_content, "events"))
+    JAXTAM_g_files = count(contains.(JAXTAM_content, "gtis"))
+
+    if JAXTAM_e_files > 0 && JAXTAM_g_files > 0 && JAXTAM_e_files == JAXTAM_g_files
+        mission_data = Dict{Symbol,InstrumentData}()
+
+        instruments = unique(replace.(JAXTAM_content, r"(_gtis|_events|.feather)", ""))
+
+        for instrument in instruments
+            info("Loading $obsid: $instrument from $JAXTAM_path")
+            inst_files = JAXTAM_content[contains.(JAXTAM_content, instrument)]
+            file_event = joinpath(JAXTAM_path, inst_files[contains.(inst_files, "events")][1])
+            file_gtis  = joinpath(JAXTAM_path, inst_files[contains.(inst_files, "gtis")][1])
+            
+            mission_data[Symbol(instrument)] = InstrumentData(instrument, Feather.read(file_event), Feather.read(file_gtis))
+        end
+    else
+        mission_data = read_cl_fits(mission_name, obs_row, obsid)
+
+        for key in keys(mission_data)
+            print("\n"); info("Saving $(string(key))")
+
+            _save_fits_feather(JAXTAM_path, mission_data[key].instrument, mission_data[key].events, mission_data[key].gti)
+        end
+    end
+
+    return mission_data
 end
