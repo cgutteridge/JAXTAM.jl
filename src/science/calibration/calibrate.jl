924a97d74d4432b7d3747493ef89fda05ff87204
Robert Rosca <robertrosca@users.noreply.github.com>
2018-07-02 23:55:45 +0100

    Improve user-facing calibrate function, saves pi energies to  instrument_calib.feather

--- src/science/calibration/calibrate.jl
+++ src/science/calibration/calibrate.jl
@@ -56,20 +56,43 @@ function _read_calibration(pis::Array{Int16,1})
 end
 
 function calibrate(mission_name::Symbol, obs_row::DataFrames.DataFrame)
+    obsid       = obs_row[:obsid][1]
     JAXTAM_path = abspath(string(obs_row[:obs_path][1], "/JAXTAM/"))
 
     JAXTAM_content = readdir(JAXTAM_path)
     JAXTAM_e_files = count(contains.(JAXTAM_content, "events"))
     JAXTAM_c_files = count(contains.(JAXTAM_content, "calib"))
 
+    calibrated_energy = Dict{Symbol,DataFrames.DataFrame}()
+
+    instruments = unique(replace.(JAXTAM_content, r"(_gtis|_events|_calib|.feather)", ""))
+
     if JAXTAM_e_files > 0 && JAXTAM_e_files > JAXTAM_c_files
-        
+        info("Loading EVENTS for $(obsid) from $JAXTAM_path")
+
+        for instrument in instruments
+            info("Loading $instrument EVENTS")
+
+            event_instrument = read_cl(mission_name, obs_row)[Symbol(instrument)].events
+            es = DataFrame(E = _read_calibration(event_instrument[:PI], mission_name))
+
+            info("Saving $instrument CALIB energy")
+
+            Feather.write(joinpath(JAXTAM_path, "$instrument\_calib.feather"), es)
+
+            calibrated_energy[Symbol(instrument)] = es
+        end
+    elseif JAXTAM_e_files > 0 && JAXTAM_e_files == JAXTAM_c_files
+        info("Loading CALIB $(obsid): from $JAXTAM_path")
+
+        for instrument in instruments
+            info("Loading $instrument CALIB energy")
+
+            calibrated_energy[Symbol(instrument)] = Feather.read(joinpath(JAXTAM_path, "$instrument\_calib.feather"))
+        end
     end
 
-    pis = read_cl(mission_name, obs_row)[:XTI].events[:PI]
-    es  = _read_calibration(pis, mission_name)
-
-    return es
+    return calibrated_energy
 end
 
 function calibrate(mission_name::Symbol, append_df::DataFrames.DataFrame, obsid::String)
@@ -81,3 +104,5 @@ end
 function calibrate(mission_name::Symbol, obsid::String)
     return calibrate(mission_name, master_a(mission_name), obsid)
 end
+
+#JAXTAM.calibrate(:nicer, "1010010192")
