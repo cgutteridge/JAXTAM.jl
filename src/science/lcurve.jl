6fb480fe2552e9ad719170dd9c440dd84112ef34
Robert Rosca <robertrosca@users.noreply.github.com>
2018-07-08 12:44:37 +0100

    Reogransie lcurve file

--- src/science/lcurve.jl
+++ src/science/lcurve.jl
@@ -18,7 +18,7 @@ struct GTIPart
     times::Array
 end
 
-function _lcurve_filter_time(event_times, event_energies, gtis, start_time, stop_time)
+function _lcurve_filter_time(event_times, event_energies, gtis, start_time, stop_time, filter_low_count_gtis=true)
     mask_good_times  = start_time .<= event_times .<= stop_time
     
     event_times    = event_times[mask_good_times]
@@ -27,6 +27,10 @@ function _lcurve_filter_time(event_times, event_energies, gtis, start_time, stop
     event_times = event_times .- start_time
     gtis = hcat(gtis[:START], gtis[:STOP])
     gtis = gtis .- start_time
+
+    counts_per_gti_sec = [count(gtis[g, 1] .<= event_times .<= gtis[g, 2])/(gtis[g, 2] - gtis[g, 1]) for g in 1:size(gtis, 1)]
+
+    println(counts_per_gti_sec)
     
     return event_times, event_energies, gtis
 end
@@ -41,11 +45,11 @@ function _lc_filter_energy(event_times, event_energies, good_energy_max, good_en
 end
 
 function _lc_bin(event_times, bin_time, time_start, time_stop)
-    if bin_time < 0
-        if (1/bin_time & ((1/bin_time)-1)) != 0
+    if bin_time < 1
+        if (Int(1/bin_time) & Int((1/bin_time)-1)) != 0
             warn("Bin time not pow2")
         end
-    elseif bin_time > 0
+    elseif bin_time > 1
         if (bin_time & (bin_time-1)) != 0
             warn("Bin time not pow2")
         end
@@ -53,54 +57,12 @@ function _lc_bin(event_times, bin_time, time_start, time_stop)
         error("Bin time cannot be zero")
     end
 
-    binned_histogram = fit(Histogram, event_times, 0:bin_time:ceil(time_stop-time_start), closed=:left)
+    binned_histogram = fit(Histogram, event_times, 0:bin_time:(time_stop-time_start), closed=:left)
 
     counts = sparse(binned_histogram.weights)
     times  = binned_histogram.edges[1][1:length(counts)]
     
     return times, counts
-end
-
-function _lc_filter_gtis(binned_times, binned_counts, gtis, time_start, time_stop, mission, instrument, obsid)
-    # Dodgy way to convert a matrix into an array of arrays
-    # so each GTI is stored as an array of [start; finish]
-    # and each of those GTI arrays is an array itself
-    # makes life a bit easier for the following `for gti in gtis` loop
-    gtis = [gtis[x, :] for x in 1:size(gtis, 1)]
-
-    gti_data = Array{GTIPart,1}()
-
-    bin_time = binned_times[2] - binned_times[1]
-
-    excluded_gti_count = 0
-
-    for (i, gti) in enumerate(gtis) # For each GTI, store the selected times and count rate within that GTI
-        start = findfirst(binned_times .>= gti[1])
-        stop  = findfirst(binned_times .>= gti[2])-1
-
-        if (stop-start)*bin_time > 30
-            append!(gti_data, [GTIPart(mission, instrument, obsid, i, start, binned_counts[start:stop], binned_times[start:stop].-gti[1])]) # Subtract GTI start time from all times, so all start from t=0
-        else
-            excluded_gti_count += 1
-        end
-    end
-
-    total_counts = sum(binned_counts)[1]
-    gti_counts   = sum([sum(gti.counts) for gti in gti_data])
-    count_delta  = gti_counts-total_counts
-    delta_prcnt  = round(count_delta/total_counts*100, 2)
-
-    info("Original counts: $total_counts, counts in GTI: $gti_counts, delta: $count_delta ($delta_prcnt %)")
-
-    if excluded_gti_count > 0
-        warn("Excluded $excluded_gti_count gtis < 30s")
-    end
-
-    if abs(count_delta) > 0.1*total_counts
-        warn("Count delta > 0.1% of total counts")
-    end
-
-    return gti_data
 end
 
 function _lcurve(instrument_data, bin_time)
@@ -140,7 +102,7 @@ function _lc_read(lc_dir::String, instrument::Symbol, bin_time)
     return BinnedData(Symbol(lc_meta[:mission][1]), Symbol(lc_meta[:instrument][1]), lc_meta[:obsid][1], lc_meta[:bin_time][1], lc_data[:counts], lc_meta[:times][1]:lc_meta[:times][2]:lc_meta[:times][3], hcat(lc_gtis[:start], lc_gtis[:stop]))
 end
 
-function lcurve(mission_name::Symbol, obs_row::DataFrame, bin_time::Number)
+function lcurve(mission_name::Symbol, obs_row::DataFrame, bin_time::Number; overwrite=false)
     obsid          = obs_row[:obsid][1]
     JAXTAM_path    = abspath(string(obs_row[:obs_path][1], "/JAXTAM/"))
     JAXTAM_content = readdir(JAXTAM_path)
@@ -155,7 +117,7 @@ function lcurve(mission_name::Symbol, obs_row::DataFrame, bin_time::Number)
 
     lightcurves = Dict{Symbol,BinnedData}()
 
-    if JAXTAM_c_files > 0 && JAXTAM_c_files > JAXTAM_lc_files
+    if (JAXTAM_c_files > 0 && JAXTAM_c_files > JAXTAM_lc_files) || (JAXTAM_c_files > 0 && overwrite)
         calibrated_data = calibrate(mission_name, obs_row)
     
         for instrument in instruments
@@ -179,10 +141,56 @@ function lcurve(mission_name::Symbol, obs_row::DataFrame, bin_time::Number)
     return lightcurves
 end
 
-function _gtis(instrument_data, bin_time)
-    binned_times, binned_counts, gtis = _lcurve(instrument_data, bin_time)
+function lcurve(mission_name::Symbol, obsid::String, bin_time::Number; overwrite=false)
+    obs_row = JAXTAM.master_query(mission_name, :obsid, obsid)
 
-    gti_data = _lc_filter_gtis(binned_times, binned_counts, gtis, instrument_data.start, instrument_data.stop, instrument_data.mission, instrument_data.instrument, instrument_data.obsid)
+    return lcurve(mission_name, obs_row, bin_time; overwrite=overwrite)
+end
+
+function _lc_filter_gtis(binned_times, binned_counts, gtis, time_start, time_stop, mission, instrument, obsid)
+    # Dodgy way to convert a matrix into an array of arrays
+    # so each GTI is stored as an array of [start; finish]
+    # and each of those GTI arrays is an array itself
+    # makes life a bit easier for the following `for gti in gtis` loop
+    gtis = [gtis[x, :] for x in 1:size(gtis, 1)]
+
+    gti_data = Array{GTIPart,1}()
+
+    bin_time = binned_times[2] - binned_times[1]
+
+    excluded_gti_count = 0
+
+    for (i, gti) in enumerate(gtis) # For each GTI, store the selected times and count rate within that GTI
+        start = findfirst(binned_times .> gti[1])
+        stop  = findfirst(binned_times .> gti[2])-1
+
+        if (stop-start)*bin_time > 30
+            append!(gti_data, [GTIPart(mission, instrument, obsid, i, start, binned_counts[start:stop], binned_times[start:stop].-gti[1])]) # Subtract GTI start time from all times, so all start from t=0
+        else
+            excluded_gti_count += 1
+        end
+    end
+
+    total_counts = sum(binned_counts)[1]
+    gti_counts   = sum([sum(gti.counts) for gti in gti_data])
+    count_delta  = gti_counts-total_counts
+    delta_prcnt  = round(count_delta/total_counts*100, 2)
+
+    info("Original counts: $total_counts, counts in GTI: $gti_counts, delta: $count_delta ($delta_prcnt %)")
+
+    if excluded_gti_count > 0
+        warn("Excluded $excluded_gti_count gtis < 30s")
+    end
+
+    if abs(count_delta) > 0.1*total_counts
+        warn("Count delta > 0.1% of total counts")
+    end
+
+    return gti_data
+end
+
+function _gtis(lc::BinnedData)
+    gti_data = _lc_filter_gtis(lc.times, lc.counts, lc.gtis, lc.times[1], lc.times[end], lc.mission, lc.instrument, lc.obsid)
 
     return gti_data
 end
