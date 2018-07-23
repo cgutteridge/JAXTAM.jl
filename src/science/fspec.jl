91748fbc72384adc806dcb38ab5d396d2468e25e
Robert Rosca <robertrosca@users.noreply.github.com>
2018-07-23 09:25:06 +0100

    Add gtis_pow2 function

--- src/science/fspec.jl
+++ src/science/fspec.jl
@@ -24,7 +24,7 @@ function _fft(counts::Array{Int64,1}, times::Array, bin_time::Real; leahy=true)
     amps[1] = 0 # Zero the 0Hz amplitude
 
     if leahy
-        amps = (2.*(amps.^2)) ./ sum(amps)
+        amps = (2.*(amps.^2)) ./ sum(counts)
     end
 
     return freqs, amps
@@ -34,13 +34,28 @@ function _best_gti_pow2(gtis::Dict{Int64,JAXTAM.GTIData})
     gti_pow2s = [prevpow2(length(gti[2].counts)) for gti in gtis]
     min_pow2  = Int(median(gti_pow2s))
     exclude   = gti_pow2s .< min_pow2
+    
+    instrument = gtis[collect(keys(gtis))[1]].instrument
 
-    info("Median pow2 length is $min_pow2, excluded $(count(exclude)) gtis, ", 
+
+    info("$instrument median pow2 length is $min_pow2, excluded $(count(exclude)) gtis, ", 
         "$(length(gti_pow2s) - count(exclude)) remain")
 
     good_gtis = [gti for gti in gtis][.!exclude]
 
     return Dict(good_gtis)
+end
+
+function gtis_pow2(mission_name::Symbol, gtis::Dict{Symbol,Dict{Int64,JAXTAM.GTIData}})
+    instruments = config(mission_name).instruments
+
+    gtis_pow2 = Dict{Symbol,Dict{Int64,JAXTAM.GTIData}}()
+
+    for instrument in instruments
+        gtis_pow2[Symbol(instrument)] = _best_gti_pow2(gtis[Symbol(instrument)])
+    end
+
+    return gtis_pow2
 end
 
 function fspec(mission_name::Symbol, gtis::Dict{Symbol,Dict{Int64,JAXTAM.GTIData}})
@@ -59,8 +74,4 @@ function fspec(mission_name::Symbol, gtis::Dict{Symbol,Dict{Int64,JAXTAM.GTIData
     end
 
     return powspecs
-end
-
-function plot(data::FFTData)
-    return plot(data.freqs, data.amps)
 end
