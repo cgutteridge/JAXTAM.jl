fe489725ef366a63fadef42701a51c22ba1c39dd
Robert Rosca <robertrosca@users.noreply.github.com>
2018-08-21 16:10:40 +0100

    Improve plotting for orbits

--- src/science/plots.jl
+++ src/science/plots.jl
@@ -4,26 +4,32 @@ function _savefig_obsdir(mission_name, obsid, bin_time, fig_name)
     savefig(joinpath(plot_dir, fig_name))
 end
 
-function plot!(data::BinnedData; lab="", size_in=(1140,400), save_plt=true)
+function plot!(data::Union{BinnedData,BinnedOrbitData}; lab="", size_in=(1140,400), save_plt=true)
     bin_time_pow2 = Int(log2(data.bin_time))
 
     Plots.plot()
 
+    if typeof(data) == BinnedData
+        plot_title = "FFT - 2^$(bin_time_pow2) - $(data.bin_time) bt"
+    elseif typeof(data) == BinnedOrbitData
+        plot_title = "FFT - 2^$(bin_time_pow2) - $(data.bin_time) bt - orbit $(data.orbit_id)"
+    end
+
     Plots.plot!(data.times, data.counts,
         xlab="Time (s)", ylab="Counts (log10)",
-        lab=lab, alpha=1, title="FFT - 2^$(bin_time_pow2) - $(data.bin_time) bt")
+        lab=lab, alpha=1, title=plot_title)
 
-    Plots.vline!(data.gtis[:, 2], lab="GTI Stop", alpha=0.75)
-
+    Plots.vline!(data.gtis[:, 2], lab="GTI Stop",  alpha=0.75)
     Plots.vline!(data.gtis[:, 1], lab="GTI Start", alpha=0.75)
 
     count_min = maximum([minimum(data.counts[data.counts != 0]), 0.1])
     count_max = maximum(data.counts)
-    yticks = range(log10(prevpow(10, count_min)), step=0.2, stop=log10(nextpow(10, count_min)))
+    log10_min = count_min > 1 ? prevpow(10, count_min) : 1/prevpow(10, 1/count_min)
+    yticks = range(log10(log10_min), stop=log10(nextpow(10, count_max)), length=5)
 
     yticks = round.(exp10.(yticks), sigdigits=3)
 
-    count_min > 1 ? ylim = (prevpow(10, count_min), nextpow(10, count_max)) : ylim = (1/prevpow(10, 1/count_min), nextpow(10, count_max))
+    ylim = (log10_min, nextpow(10, count_max))
     yaxis!(yscale=:log10, yticks=yticks, ylims=ylim)
 
     try
@@ -56,14 +62,14 @@ function plot(instrument_data::Dict{Symbol,JAXTAM.BinnedData}; size_in=(1140,400
     return Plots.plot!(size=size_in)
 end
 
-function plot!(data::FFTData; lab="", size_in=(1140,600), save_plt=true, norm=:RMS, rebin=rebin, logx=true, logy=true)
+function plot!(data::FFTData; lab="", size_in=(1140,600), save_plt=true, norm=:RMS, rebin=(:log10, 0.01), logx=true, logy=true)
     bin_time_pow2 = Int(log2(data.bin_time))
 
     # Don't plot the 0Hz amplitude
     avg_amp = data.avg_amp
     freqs   = data.freqs
     avg_amp[1] = NaN
-    freq_min   = freqs[1] + eps() # Make it not be exactly equal to zero
+    freq_min   = freqs[2] # Skip zero freq
     freqs[1]   = NaN
 
     freqs, avg_amp = fspec_rebin(avg_amp, freqs; rebin=rebin)
@@ -93,7 +99,7 @@ function plot!(data::FFTData; lab="", size_in=(1140,600), save_plt=true, norm=:R
     end
     
     Plots.plot!(xlab="Freq (Hz)", alpha=1,
-        title="FFT - $(data.obsid) - 2^$(bin_time_pow2) bt - $(data.bin_size*data.bin_time) bs")
+        title="FFT - $(data.obsid) - 2^$(bin_time_pow2) bt - $(data.bin_size*data.bin_time) bs - $rebin rebin")
 
     if(save_plt)
         _savefig_obsdir(data.mission, data.obsid, data.bin_time, "fspec.png")
