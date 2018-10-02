ac3280cd168c26224a081722b87c717f28ae90e1
Robert Rosca <robertrosca@users.noreply.github.com>
2018-10-02 18:13:26 +0100

    Hopefully enable docs

--- docs/make.jl
+++ docs/make.jl
@@ -9,6 +9,10 @@ makedocs(
     modules = [JAXTAM]
 )
 
+deploydocs(
+    repo = "github.com/RobertRosca/JAXTAM.jl.git",
+)
+
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
